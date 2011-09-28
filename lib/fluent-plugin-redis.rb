0a1220dc777bf1f12f8d17f5f8c86fd295514716
Yuki Nishijima <mail@yukinishijima.net>
2011-09-29 01:32:48 +0900

    first commit.

---
+++ lib/fluent-plugin-redis.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Redis
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent-plugin-redis/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Redis
+      VERSION = "0.0.1"
+    end
+  end
+end
