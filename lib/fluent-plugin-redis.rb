d29310f086deebda72e32f16769e3b82e617c5c8
Yuki Nishijima <mail@yukinishijima.net>
2011-09-30 16:30:21 +0900

    removed unnecessary files.

--- lib/fluent-plugin-redis.rb
+++
@@ -1,7 +0,0 @@
-module Fluent
-  module Plugin
-    module Redis
-      # Your code goes here...
-    end
-  end
-end
--- lib/fluent-plugin-redis/version.rb
+++
@@ -1,7 +0,0 @@
-module Fluent
-  module Plugin
-    module Redis
-      VERSION = "0.0.1"
-    end
-  end
-end
