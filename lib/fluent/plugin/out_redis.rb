ed0ceadb1246e0f709d711400b8682a1bc5fdf6c
Hiroshi Hatake <hatake@clear-code.com>
2016-12-01 16:10:58 +0900

    Enfoce to use new hash syntax
        % rubocop --only HashSyntax --auto-correct

--- lib/fluent/plugin/out_redis.rb
+++ lib/fluent/plugin/out_redis.rb
@@ -12,10 +12,10 @@ module Fluent::Plugin
 
     attr_reader :redis
 
-    config_param :host, :string, :default => 'localhost'
-    config_param :port, :integer, :default => 6379
-    config_param :db_number, :integer, :default => 0
-    config_param :password, :string, :default => nil, :secret => true
+    config_param :host, :string, default: 'localhost'
+    config_param :port, :integer, default: 6379
+    config_param :db_number, :integer, default: 0
+    config_param :password, :string, default: nil, secret: true
 
     config_section :buffer do
       config_set_default :@type, DEFAULT_BUFFER_TYPE
@@ -35,10 +35,10 @@ module Fluent::Plugin
       super
 
       options = {
-        :host => @host,
-        :port => @port,
-        :thread_safe => true,
-        :db => @db_number
+        host: @host,
+        port: @port,
+        thread_safe: true,
+        db: @db_number
       }
       options[:password] = @password if @password
 
